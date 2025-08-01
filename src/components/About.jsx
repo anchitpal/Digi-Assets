ed69ee3fe79efaf0e8b1862d0ce53575efedc129
Anchit Pal <anchitpal760@gmail.com>
2025-08-02 00:59:44 +0530

    Completed Home Page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,7 @@ const About = () => {
     <>
     <Layout>
     <div>
-      Heyy I am About
-      <p>This is the About page of Digi-Commerce platform.</p>
+      
     </div>
     </Layout>
     </>
