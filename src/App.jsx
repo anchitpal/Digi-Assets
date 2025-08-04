0367057e261fcdc832d929a3398b2dc72def8005
Anchit Pal <anishpal760@gmail.com>
2025-08-04 22:59:28 +0530

    Adding half Marketplace Page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Home from './components/Home'
 import Login from './components/Login'
 import About from './components/About'
 import Contact from './components/Contact'
+import MarketPlace from './components/MarketPlace'
 
 function App() {
   return (
@@ -21,6 +22,8 @@ function App() {
         <Route path='/login' element={<Login/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
+        <Route path='/marketplace' element={<MarketPlace/>} />
+        <Route path='/upload' element={<h1 className='text-3xl text-center mt-20'>Upload Page Coming Soon!</h1>} />
       </Routes>
     </Router>
     </>
